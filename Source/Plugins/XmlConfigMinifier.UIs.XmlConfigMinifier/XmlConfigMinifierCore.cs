76e3b8f8d97d627272b92e00680d16fbb537d81c
PeterKottas <peter.kottas@petfre.gi>
2016-05-21 11:24:11 +0200

    Most of the work done

---
+++ Source/Plugins/XmlConfigMinifier.UIs.XmlConfigMinifier/XmlConfigMinifierCore.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace XmlConfigMinifier.UIs.XmlConfigMinifier
+{
+    public static class XmlConfigMinifierCore
+    {
+        public static string Minify(
+            string xmlString,
+            int indention,
+            bool removeComments,
+            bool preserveWhiteSpace)
+        {
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.PreserveWhitespace = preserveWhiteSpace;
+            xDoc.LoadXml(xmlString);
+            if (removeComments)
+            {
+                XmlNodeList list = xDoc.SelectNodes("//comment()");
+                foreach (XmlNode node in list)
+                {
+                    node.ParentNode.RemoveChild(node);
+                }
+            }
+
+            string xml;
+            using (StringWriter sw = new StringWriter())
+            {
+                using (XmlTextWriter xtw = new XmlTextWriter(sw))
+                {
+                    if (indention > 0)
+                    {
+                        xtw.IndentChar = ' ';
+                        xtw.Indentation = indention;
+                        xtw.Formatting = System.Xml.Formatting.Indented;
+                    }
+
+                    xDoc.WriteContentTo(xtw);
+                    xtw.Close();
+                    sw.Close();
+                }
+                xml = sw.ToString();
+            }
+
+            return xml;
+        }
+    }
+}
---
+++ Source/UIs/XmlConfigMinifier.UIs.Console/Program.cs
@@ -0,0 +1,94 @@
+﻿using Mono.Options;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using XmlConfigMinifier.UIs.XmlConfigMinifier;
+
+namespace XmlConfigMinifier.UIs.ConsoleApp
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            int verbosity = 0;
+            var whitespaces = false;
+            var indentation = 1;
+            bool showHelp = false;
+            bool removeComments = true;
+            List<string> fileNames = new List<string>();
+            int repeat = 1;
+
+            var p = new OptionSet() {
+                { "file|f=", "Path to Xml config file/s to minify. For multiple files use double quotes and separate with commas.",
+                  (string v) => fileNames.AddRange (v.Split(',')) },
+                { "whitespace|w=", "include whitespaces (true|false). Default == false.",
+                  (bool v) => whitespaces = v },
+                { "indentation|i=", "config indentation. Default == 1.",
+                  (int v) => indentation = v },
+                { "v|verbose", "increase debug message verbosity",
+                    v => { if (v != null) verbosity=1; } },
+                { "h|help",  "show this message and exit", 
+                  v => showHelp = v != null },
+                { "c|comments",  "remove comments (true|false). Default == true.", 
+                  (bool v) => removeComments = v != null },
+            };
+
+            try
+            {
+                var extra = p.Parse(args);
+                Debug(verbosity,"We've received some extra parameters : {0}",string.Concat(extra));
+            }
+            catch (OptionException e)
+            {
+                Console.Write("That didn't work: ");
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Try -help' for more information.");
+                return;
+            }
+
+            if (showHelp)
+            {
+                ShowHelp(p);
+                return;
+            }
+
+            foreach (string fileName in fileNames)
+            {
+                for (int i = 0; i < repeat; ++i)
+                {
+                    Console.WriteLine("Starting to minify {0}", fileName);
+                    var xmlString = string.Empty;
+                    try
+                    {
+                        xmlString = File.ReadAllText(fileName);
+                        XmlConfigMinifierCore.Minify(xmlString, indentation, removeComments, whitespaces);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Cannot minify  because of an exception : {0}", e.Message);
+                    }
+                }
+            }
+        }
+
+        static void ShowHelp(OptionSet p)
+        {
+            Console.WriteLine("This tool is used to minify web.config file.");
+            Console.WriteLine("Use options to achieve custom indention, remove whitespaces, comments etc.");
+            Console.WriteLine();
+            Console.WriteLine("Options:");
+            p.WriteOptionDescriptions(Console.Out);
+        }
+
+        static void Debug(int verbosity, string format, params object[] args)
+        {
+            if (verbosity > 0)
+            {
+                Console.Write("# ");
+                Console.WriteLine(format, args);
+            }
+        }
+    }
+}
